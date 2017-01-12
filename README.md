f3dd4f62a0d4e5b815e543976395cdc50f5be9b7
TTaiN <thomatn5@uci.edu>
2017-01-12 10:03:09 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# CS122B (Projects in Databases and Web Applications) 
+Winter 2017 | Group 42
+
+# Team Members
+https://github.com/TTaiN
+
+# Project Slack
+()
+
+# Project Googledoc
+()
---
+++ db/createtable.sql
@@ -0,0 +1,38 @@
+﻿/*
+  Thomas T Nguyen
+  CS122B Winter 2017 
+  Project Database Script
+*/
+
+CREATE DATABASE IF NOT EXISTS moviedb;
+
+USE moviedb;
+
+CREATE TABLE movies(id INTEGER AUTO_INCREMENT NOT NULL, title VARCHAR(100) NOT NULL, year INTEGER NOT NULL,
+  director VARCHAR(100) NOT NULL, banner_url VARCHAR(200) DEFAULT '', trailer_url VARCHAR(200) DEFAULT '',
+  PRIMARY KEY (id));
+
+CREATE TABLE stars(id INTEGER AUTO_INCREMENT NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL,
+  dob DATE, photo_url VARCHAR(200) DEFAULT '',
+  PRIMARY KEY (id));
+
+CREATE TABLE stars_in_movies(star_id INTEGER NOT NULL, movie_id INTEGER NOT NULL,
+  FOREIGN KEY (star_id) REFERENCES stars(id), FOREIGN KEY (movie_id) REFERENCES movies(id)); /* Primary key might not be necessary. */
+
+CREATE TABLE genres(id INTEGER AUTO_INCREMENT NOT NULL, name VARCHAR(32) NOT NULL,
+  PRIMARY KEY (id));
+
+CREATE TABLE genres_in_movies(genre_id INTEGER NOT NULL, movie_id INTEGER NOT NULL,
+  FOREIGN KEY (genre_id) REFERENCES genres(id), FOREIGN KEY (movie_id) REFERENCES movies(id));
+
+/* Credit card table required before customers. */
+
+CREATE TABLE creditcards (id VARCHAR(20) NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, expiration DATE NOT NULL,
+  PRIMARY KEY (id));
+
+CREATE TABLE customers(id INTEGER AUTO_INCREMENT NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL,
+  cc_id VARCHAR(20) NOT NULL, address VARCHAR(200) NOT NULL, email VARCHAR(50) NOT NULL, `password` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id), FOREIGN KEY (cc_id) REFERENCES creditcards(id));
+
+CREATE TABLE sales(id INTEGER AUTO_INCREMENT NOT NULL, customer_id INTEGER NOT NULL, movie_id INTEGER NOT NULL, sale_date DATE NOT NULL,
+  PRIMARY KEY (id), FOREIGN KEY (customer_id) REFERENCES customers(id), FOREIGN KEY (movie_id) REFERENCES movies(id));
