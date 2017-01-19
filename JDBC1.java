16e8ea3009678075df39b8a4642f3442d6e5b68d
Mohammed Alabdullatif <molatif@outlook.com>
2017-01-18 19:28:38 -0800

    Update JDBC1.java

--- JDBC1.java
+++ JDBC1.java
@@ -1,6 +1,4 @@
-// JDBC Example - printing a database's metadata
-// Coded by Chen Li/Kirill Petrov Winter, 2005
-// Slightly revised for ICS185 Spring 2005, by Norman Jacobson
+// JDBC1
 
 import java.sql.*; // Enable SQL processing
 import java.util.Scanner;
