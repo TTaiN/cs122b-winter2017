a59e19633d01a749e114ff04486edc33797246a6
Pupgrammer <Pupgrammer@github.com>
2017-01-24 14:51:06 -0800

    updated db mistake

--- db/createtable.sql
+++ db/createtable.sql
@@ -13,13 +13,13 @@ CREATE TABLE stars(id INTEGER AUTO_INCREMENT NOT NULL, first_name VARCHAR(50) NO
   PRIMARY KEY (id));
 
 CREATE TABLE stars_in_movies(star_id INTEGER NOT NULL, movie_id INTEGER NOT NULL,
-  FOREIGN KEY (star_id) REFERENCES stars(id), FOREIGN KEY (movie_id) REFERENCES movies(id)); /* Primary key might not be necessary. */
+  FOREIGN KEY (star_id) REFERENCES stars(id), FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE); /* Primary key might not be necessary. */
 
 CREATE TABLE genres(id INTEGER AUTO_INCREMENT NOT NULL, name VARCHAR(32) NOT NULL,
   PRIMARY KEY (id));
 
 CREATE TABLE genres_in_movies(genre_id INTEGER NOT NULL, movie_id INTEGER NOT NULL,
-  FOREIGN KEY (genre_id) REFERENCES genres(id), FOREIGN KEY (movie_id) REFERENCES movies(id));
+  FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE, FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE);
 
 /* Credit card table required before customers. */
 
@@ -28,7 +28,7 @@ CREATE TABLE creditcards (id VARCHAR(20) NOT NULL, first_name VARCHAR(50) NOT NU
 
 CREATE TABLE customers(id INTEGER AUTO_INCREMENT NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL,
   cc_id VARCHAR(20) NOT NULL, address VARCHAR(200) NOT NULL, email VARCHAR(50) NOT NULL, `password` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (id), FOREIGN KEY (cc_id) REFERENCES creditcards(id));
+  PRIMARY KEY (id), FOREIGN KEY (cc_id) REFERENCES creditcards(id) ON DELETE CASCADE);
 
 CREATE TABLE sales(id INTEGER AUTO_INCREMENT NOT NULL, customer_id INTEGER NOT NULL, movie_id INTEGER NOT NULL, sale_date DATE NOT NULL,
-  PRIMARY KEY (id), FOREIGN KEY (customer_id) REFERENCES customers(id), FOREIGN KEY (movie_id) REFERENCES movies(id));
+  PRIMARY KEY (id), FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE, FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE);
