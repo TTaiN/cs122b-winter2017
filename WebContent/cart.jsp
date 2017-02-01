4b9fe8c7f0464c23c0109ece6f5d9ec2d6aa27b2
TTaiN <thomatn5@uci.edu>
2017-01-31 23:06:45 -0800

    more progress on cart

--- WebContent/cart.jsp
+++ WebContent/cart.jsp
@@ -1,8 +1,9 @@
-<%@ page import="java.util.HashMap" %>
-
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-    
+
+<%@ page import="java.util.LinkedHashMap" %>
+<%@ page import="util.Movie" %>
+
 <%
 	if (session.getAttribute("username") == null)
 	{
@@ -21,24 +22,38 @@
 <p>Welcome to your cart, ${sessionScope.username}!</p> <!--  Change to FirstName,LastName later  -->
 <table>
 	<tr>
-	    <th>Movie ID</th>
-	    <th>Title (Year)</th>
-	    <th>Director</th>
-	    <th>Image</th>
-	    <th>Quantity</th>
-	    <th>Subtotal</th>
+	    <th class='info'>Movie ID</th>
+	    <th class='info'>Image</th>
+	    <th class='info'>Title (Year)</th>
+	    <th class='info'>Director</th>
+	    <th class='info'>Quantity</th>
 	</tr>
 	<%
-		if (session.getAttribute("cart") == null)
+		LinkedHashMap<Movie, Integer> cart = (LinkedHashMap<Movie, Integer>) session.getAttribute("cart");
+
+		if (cart == null || cart.isEmpty())
 		{
-			out.println("You currently have nothing in your cart. Purchase something!");
+			out.println("<td><span>You currently have nothing in your cart. Purchase something!</span></td>");
 		}
 		else
 		{
-			HashMap<Integer, Integer> cart = (HashMap<Integer, Integer>) session.getAttribute("cart");
+			for (Movie current : cart.keySet())
+			{
+				out.println("<tr>");
+				out.println("<td class='info'>" + current.getId() + "</td>");
+				out.println("<td class='info'><img class='banner' src='" + current.getBannerUrl() + "'/></td>");
+				out.println("<td class='info'>" + current.getTitle() + " (" + current.getYear() + ")" + "</td>");
+				out.println("<td class='info'>" + current.getDirector() + "</td>");
+				out.println("<td class='info'>");
+				out.println("<span>" + cart.get(current) + " order(s)</span><br><br>");
+				out.println("<form class='quantity' action='./cart' method='post'>");
+				out.println("<input type='number' name='quantity" + current.getId() + "' value='" + cart.get(current) + "'/>");
+				out.println("<input class='submit' type='submit' value='Update Quantity'/>");
+				out.println("</form>");
+				out.println("</td>");
+			}
 		}
 	%>
 </table>
-
 </body>
 </html>
--- src/Cart.java
+++ src/Cart.java
@@ -5,22 +5,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import util.Movie;
 
 
 @WebServlet("/cart")
 
-public class Cart extends HttpServlet {
+public class Cart extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
 
-    public Cart() {
+    public Cart() 
+    {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		// TODO Auto-generated method stub
+		response.setContentType("text/html");
 		HttpSession session = request.getSession();
 		String username = (String) session.getAttribute("username");
 		if (username == null)
@@ -28,21 +30,26 @@ public class Cart extends HttpServlet {
 			response.sendRedirect("./login");
 		}
 		
-		HashMap<Integer, Integer> cart = (HashMap<Integer, Integer>) session.getAttribute("cart");
+		LinkedHashMap<Movie, Integer> cart = (LinkedHashMap<Movie, Integer>) session.getAttribute("cart");
 		if (cart == null)
 		{
-			//session.setAttribute("cart", new HashMap<Integer, Integer>());
+			session.setAttribute("cart", new LinkedHashMap<Movie, Integer>());
+			//response.getWriter().println("[DEBUG] New cart made.");
+			cart = (LinkedHashMap<Movie, Integer>) session.getAttribute("cart");
+			cart.put(new Movie(1, "JSPs Are Awesome", 2017, "Thomas T Nguyen", "http://ia.imdb.com/media/imdb/01/I/95/71/38m.jpg", "http://ia.imdb.com/media/imdb/01/I/95/71/38m.jpg"), 1);
+			cart.put(new Movie(2, "Donald Trump: The Dictator", 2015, "Akito T Nguyen", "http://ia.imdb.com/media/imdb/01/I/94/55/51m.jpg", "http://ia.imdb.com/media/imdb/01/I/94/55/51m.jpg"), 1);
+			cart.put(new Movie(4, "United States Doomed Forever?!", 2016, "TTaiN T Nguyen", "http://ia.imdb.com/media/imdb/01/I/47/24/43m.jpg", "http://ia.imdb.com/media/imdb/01/I/47/24/43m.jpg"), 1);
+			cart.put(new Movie(3, "Why I Hate Project Courses 101", 2014, "Pupcorn T Nguyen", "http://ia.imdb.com/media/imdb/01/I/34/82/91m.jpg", "http://ia.imdb.com/media/imdb/01/I/34/82/91m.jpg"), 1);
 		}
+		else
+		{
+			//response.getWriter().println("[DEBUG] Cart already exists.");
+		}
+		request.getRequestDispatcher("/cart.jsp").include(request, response);
 
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
-	
-	void printCart(HashMap<Integer, Integer> cart)
-	{
-		
 	}
 }
