a753165918b3356f483d9b5bbdc6bc763b051825
TTaiN <thomatn5@uci.edu>
2017-02-06 19:55:41 -0800

    code optimization

--- src/ecommerce_helpers/OrderDB.java
+++ src/ecommerce_helpers/OrderDB.java
@@ -116,28 +116,14 @@ public class OrderDB
 		
 		if (results.next())
 		{
-			if (!results.getString("first_name").equals(this.firstName))
+			if (!results.getString("first_name").equals(this.firstName) || !results.getString("last_name").equals(this.lastName) || !results.getString("expiration").equals(this.date))
 			{
 				messages.add("[ERROR] The information provided did not match any entries in our records.");
-				//messages.add("[ERROR] The First Name (" + this.firstName + ") did not the match the first name for the Credit Card Number (" + this.number + ") in our records.");
-			}
-			else if (!results.getString("last_name").equals(this.lastName)) // changed to else if for now
-			{
-				messages.add("[ERROR] The information provided did not match any entries in our records.");
-
-				//messages.add("[ERROR] The Last Name (" + this.lastName + ") did not the match the last name for the Credit Card Number (" + this.number + ") in our records.");
-			}
-			else if (!results.getString("expiration").equals(this.date))
-			{
-				messages.add("[ERROR] The information provided did not match any entries in our records.");
-
-				//messages.add("[ERROR] The Expiration Date (" + this.date + ") did not the match the expiration date for the Credit Card Number (" + this.number + ") in our records.");
 			}
 		}
 		else
 		{
 			messages.add("[ERROR] The information provided did not match any entries in our records.");
-			//messages.add("[ERROR] The Credit Card Number (" + this.number + ") was not found in our records.");
 		}
 		
 		if (messages.isEmpty())
