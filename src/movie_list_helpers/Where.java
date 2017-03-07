6272788cbceacfb5bf08b27dad25d88b54140273
TTaiN <thomatn5@uci.edu>
2017-03-06 22:13:41 -0800

    sd

--- src/movie_list_helpers/Where.java
+++ src/movie_list_helpers/Where.java
@@ -4,8 +4,11 @@ public class Where {
 	// Translate string to where statement
 	public static String getWhere( String str, String attr )
 	{
+		if(str.trim().equals("") || str == null)
+			return attr + " like %%";
+			
 		String words[] = str.split(" ");
-		String where = "";
+		String where = "(";
 		for( int i = 0; i < words.length; i++)
 		{
 			if( i == words.length - 1)
@@ -13,6 +16,8 @@ public class Where {
 			else
 				where = where + " " + attr + " like '%" + words[i] + "%' and";
 		}
+		where = where + ") OR (edth(" + attr + ", " + str + ", 2))";
+		System.out.println(where);
 		return where;
 	}
 
