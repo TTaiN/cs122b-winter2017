cc0699dfdcee6be43df2a76015bbf3d9b8c98183
TTaiN <thomatn5@uci.edu>
2017-03-08 20:16:55 -0800

    test

--- WebContent/script/popup.js
+++ WebContent/script/popup.js
@@ -12,11 +12,37 @@ $( function() {
          at: "center top",
          collision: "flipfit"
       },
-       show: {
-           duration: 200
-       },
+       show: null, // show immediately
        hide: {
     	   delay: 800
+       },
+       open: function(event, ui) // Credit = http://stackoverflow.com/questions/16660576/only-close-tooltip-if-mouse-is-not-over-target-or-tooltip
+       {
+           if (typeof(event.originalEvent) === 'undefined')
+           {
+               return false;
+           }
+
+           var $id = $(ui.tooltip).attr('id');
+
+           // close any lingering tooltips
+           $('div.ui-tooltip').not('#' + $id).remove();
+
+           // ajax function to pull in data and add it to the tooltip goes here
+       },
+       close: function(event, ui)
+       {
+           ui.tooltip.hover(function()
+           {
+               $(this).stop(true).fadeTo(400, 1); 
+           },
+           function()
+           {
+               $(this).fadeOut('400', function()
+               {
+                   $(this).remove();
+               });
+           });
        }
     });
 } );
