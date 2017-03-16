7e5449e6c376923f0eae5039cadf9e7524ae2fc0
TTaiN <thomatn5@uci.edu>
2017-03-16 03:01:09 -0700

    added dashboard portal

---
+++ WebContent/jsp/dashboardPortal.jsp
@@ -0,0 +1,67 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+
+<%
+	if (session.getAttribute("employee") != null || request.getAttribute("jsp") == null)
+	{
+		response.sendRedirect("../_dashboard");
+		return;
+	}
+%>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard Login</title>
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+	<style>
+		.topmenu
+		{
+		     margin: 0 auto;
+		     width: 350px; 
+		}
+		
+		.topmenu_table
+		{
+		   border: 1px solid black;
+		   text-align:center;
+		   width: 350px;
+		}
+		
+		.topmenu_button
+		 {
+		   border: 1px black;
+		   border-right: 1px black;
+		   text-align:center;
+		   padding:3px;
+		 }
+		 
+		 .topmenu_caption
+		 {
+		     font-size:110%;
+		 }
+		
+		body 
+		{
+		    background-color: rgb(253,245,230);
+			background-size: cover;
+			background-repeat: no-repeat;
+		}
+	</style>
+</head>
+
+<body>
+	<br>
+	<center><img src="http://ttain.tk/logo.png" alt="logo"></center>
+	<br>
+	<h1 align="center">Dashboard Login</h1>
+	<%= request.getAttribute("error") == null ? "" : "<center><p style='color:red;'>" + request.getAttribute("error") + "</p></center>" %>
+	<div align="center">
+		<form method="POST">
+			E-mail: <input type="text" name="email" ><br>
+			Password: <input type="password" name="pwd" ><br>
+			<input type="submit" value="Login"><br><br>
+			<div class="g-recaptcha" data-sitekey="6LdaxxUUAAAAAC9gQgGvJoqT1bAUTcDviA4zCB_t"></div>
+		</form>
+	</div>
+</body>
+</html>
---
+++ src/DashboardPortal.java
@@ -0,0 +1,102 @@
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import recaptcha_helpers.VerifyRecaptcha;
+import general_helpers.DatabaseHelper;
+
+@WebServlet("/_dashboard/")
+public class DashboardPortal extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+
+    public DashboardPortal() 
+    {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+    	response.setContentType("text/html");
+        
+        HttpSession session = request.getSession();
+        String employee = (String) session.getAttribute("employee");
+        String username = (String) session.getAttribute("username");
+        
+        if (employee == null && username == null)
+        {
+			request.setAttribute("jsp", true);
+            request.getRequestDispatcher("/jsp/dashboardPortal.jsp").include(request, response);
+        }
+        else if (employee == null && username != null)
+        {
+        	response.sendRedirect("./main");
+        }
+        else 
+        {
+        	response.sendRedirect("/fabflix/dashboardMain");
+        }
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		response.setContentType("text/html");
+   	 	HttpSession session = request.getSession(); 
+   	 	try
+   	 	{
+   	 		DatabaseHelper db = new DatabaseHelper();
+
+   	 		String email = request.getParameter("email");
+   	 		String pwd = request.getParameter("pwd");
+   	 		String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
+   	 		boolean verify = VerifyRecaptcha.verify(gRecaptchaResponse);
+   	 		
+   	 		Connection conn = db.getConnection();
+   	 		String query = "SELECT * from employees where email = ? and password = ?;";
+   	 		PreparedStatement statement = conn.prepareStatement(query);
+   	 		statement.setString(1, email);
+   	 		statement.setString(2, pwd);
+   	 		ResultSet rs = statement.executeQuery();
+   	 		
+   	 		if (verify == false)
+   		 	{
+   			 	request.setAttribute("error", "Missed Captcha.");
+   			 	request.setAttribute("jsp", true);
+   			 	request.getRequestDispatcher("/jsp/dashboardPortal.jsp").include(request, response);
+   		 	}
+   	 		else if (rs.next())
+			{
+				 session.setAttribute("employee", email);
+		   		 rs.close();
+		   		 db.closeConnection();
+				 response.sendRedirect("/fabflix/dashboardMain");
+			}
+			else
+			{
+				 request.setAttribute("error", "Invalid e-mail and/or password.");
+				 request.setAttribute("jsp", true);
+				 request.getRequestDispatcher("/jsp/dashboardPortal.jsp").include(request, response);
+			}
+			 rs.close();
+			 db.closeConnection();
+		 }
+		 catch (Exception e)
+		 {  
+			 response.getWriter().print(e.getMessage());
+			//ArrayList<String> messages = new ArrayList<String>();
+			//messages.add("There was an error when trying to login.");
+			//request.setAttribute("reason", "Login");
+			//request.setAttribute("messages", messages);
+			//request.getRequestDispatcher("./jsp/error.jsp").include(request, response);
+		 }
+	}
+}
+
